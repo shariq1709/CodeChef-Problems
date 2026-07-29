90e734820cf795478ecab0cfbe71fe7f0ec9aad8
shariq1709 <shariq1709@gmail.com>
2026-07-29 21:34:11 +0530

    ✅ PRDEXC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/PRDEXC/solution.py
+++ codechef/medium/PRDEXC/solution.py
@@ -1,11 +1,11 @@
-# cook your dish here
-T=int(input())
-for i in range(T):
-    X,Y,P=map(int,input().split())
-    if X*Y>=P:
+import math
+
+T = int(input())
+for _ in range(T):
+    X, Y, P = map(int, input().split())
+    if X * Y >= P:
         print(0)
     else:
-        for i in range(1,P+1):
-            if (X+i)*Y>=P or (X)*(Y+i)>=P:
-                print(i)
-                break
+        incX = math.ceil(P / Y) - X
+        incY = math.ceil(P / X) - Y
+        print(min(incX, incY))
