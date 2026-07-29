0a110900aaad4f7366735fcfe619214b243d59fa
shariq1709 <shariq1709@gmail.com>
2026-07-29 11:43:22 +0530

    ✅ LPYAS10B | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/LPYAS10B/solution.py
+++ codechef/medium/LPYAS10B/solution.py
@@ -1,12 +1,6 @@
-def calculate_area(length, width):
-    # Write your code here
-    result=length*width
-    return result
-    
-def main():
-    length, width = map(int, input().split())
-    area = calculate_area(length, width)
-    print(area)
-
-
-main()
+# cook your dish here
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
