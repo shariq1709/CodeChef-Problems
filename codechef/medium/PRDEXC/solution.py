332792c74fe2fc9ab0ed23edd75a894b2eeab979
shariq1709 <shariq1709@gmail.com>
2026-07-29 21:29:26 +0530

    ✅ PRDEXC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/PRDEXC/solution.py
+++ codechef/medium/PRDEXC/solution.py
@@ -2,10 +2,10 @@
 T=int(input())
 for i in range(T):
     X,Y,P=map(int,input().split())
-    if X*Y>=10:
+    if X*Y>=P:
         print(0)
     else:
         for i in range(1,P+1):
-            if (X+i)*Y>=10 or (X)*(Y+i)>=10:
+            if (X+i)*Y>=P or (X)*(Y+i)>=P:
                 print(i)
                 break
