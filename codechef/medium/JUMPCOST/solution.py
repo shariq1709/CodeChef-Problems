e05a2702922be139ebc2b35c5ef18b465a85b6bf
shariq1709 <shariq1709@gmail.com>
2026-08-05 21:08:45 +0530

    ✅ JUMPCOST | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/JUMPCOST/solution.py
+++ codechef/medium/JUMPCOST/solution.py
@@ -1,7 +1,18 @@
 # cook your dish here
-T=int(input())
+T = int(input())
 for i in range(T):
-    N=int(input())
-    arr=list(map(int,input().split()))
-    balance=0
+    N = int(input())
+    arr = list(map(int, input().split()))
     
+    max_bal = 0
+    pos_sum = 0
+    
+    for j in range(1, N):
+        current_bal = arr[j] - j + pos_sum
+        
+        if current_bal > max_bal:
+            max_bal = current_bal
+        if arr[j] > 0:
+            pos_sum += arr[j]
+            
+    print(max_bal)
