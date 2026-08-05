f6aa6558b4127efd6a63207cf7ff15d80d1d6d38
shariq1709 <shariq1709@gmail.com>
2026-08-05 20:22:16 +0530

    ✅ SUBCOST7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/SUBCOST7/solution.py
+++ codechef/medium/SUBCOST7/solution.py
@@ -2,7 +2,8 @@
 T=int(input())
 for i in range(T):
     N,X,Y=map(int,input().split())
-    if N<=3:
+    total=(3*X)+((N-3)*Y)
+    if N>=3:
         print(N*X)
     else:
-        print((3*X)+((N-3)*Y))
+        print(total)
