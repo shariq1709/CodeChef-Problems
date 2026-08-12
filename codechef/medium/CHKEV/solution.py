f59753801cad607810f979c4e7d66792ae5dbb48
shariq1709 <shariq1709@gmail.com>
2026-08-12 20:07:32 +0530

    ✅ CHKEV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHKEV/solution.py
+++ codechef/medium/CHKEV/solution.py
@@ -1,2 +1,11 @@
 # cook your dish here
 L,R=map(int,input().split())
+flag=0
+for i in range(L,R+1):
+    if i%2==0:
+        flag=1
+        break
+if flag==1:
+    print("Yes")
+else:
+    print("No")
