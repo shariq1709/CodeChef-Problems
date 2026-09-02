87c00d37918449481faecd1fddc233b38768213c
shariq1709 <shariq1709@gmail.com>
2026-09-02 20:34:09 +0530

    ✅ MAXSUM77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.py
+++ codechef/medium/MAXSUM77/solution.py
@@ -1,10 +1,10 @@
 # cook your dish here
 T=int(input())
-for i in range(T):
+for _ in range(T):
     N,K=map(int,input().split())
     arr=list(map(int,input().split()))
     new=sorted(arr)
-    new[::-1]
+    new=new[::-1]
     for i in range(1,K+1):
         new.pop()
     print(sum(new))
