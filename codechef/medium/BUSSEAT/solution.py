eee4a23fae07b4d2f0ed155b97ed87ac3d030eb9
Mohammed Shariq Zama <shariq1709@gmail.com>
2026-09-16 20:12:22 +0530

    ✅ BUSSEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.py
+++ codechef/medium/BUSSEAT/solution.py
@@ -1,12 +1,7 @@
-# Read the number of test cases
 T = int(input())
-
-for _ in range(T):
+for i in range(T):
     N, K = map(int, input().split())
-    
-    # If K is less than or equal to N, everyone gets an empty row.
-    # Otherwise, K - N people will have to share a row.
     if K <= N:
         print(0)
     else:
-        print(K - N)
+        print(2*(K - N))
